48131d2e5406cad1482bf2bc87301d632f26dd3d
Eric <lderic0415@gmail.com>
2023-11-09 13:10:47 -0800

    add index.ts

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import Listener from "./listener";
+import OptimizedEvents from "./main";
+import { Callback, Level } from "./types";
+
+export default OptimizedEvents;
+
+export {
+    Listener,
+    Callback,
+    Level
+};
