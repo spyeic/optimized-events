3a427a355d94f23c5935d297971b082b39a53578
Eric <lderic0415@gmail.com>
2023-11-11 15:15:45 -0800

    turn off linebreak-style

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,10 +24,7 @@ module.exports = {
             "error",
             4
         ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
+        "linebreak-style": "off",
         "quotes": [
             "error",
             "double"
