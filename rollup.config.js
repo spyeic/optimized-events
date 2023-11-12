06ab0d4db2f89873f380d56accfe2b7746537ab4
Eric <lderic0415@gmail.com>
2023-11-11 21:55:25 -0800

    add commonjs dist

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+
+export default {
+    input: "src/index.ts",
+    output: [
+        {
+            file: "dist/cjs/index.js",
+            format: "cjs"
+        },
+        {
+            file: "dist/esm/index.js",
+            format: "esm"
+        }
+    ],
+    plugins: [
+        typescript(),
+        commonjs()
+    ]
+};
