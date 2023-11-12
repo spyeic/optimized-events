8b05c96db162fd6d7981cdd80ac2876f5b5e7495
Eric <lderic0415@gmail.com>
2023-11-11 22:06:38 -0800

    add types

---
+++ types/index.d.ts
@@ -0,0 +1,5 @@
+import Listener from "./listener";
+import OptimizedEvents from "./main";
+import { Callback, Level } from "./types";
+export default OptimizedEvents;
+export { Listener, Callback, Level };
---
+++ types/types.d.ts
@@ -0,0 +1,6 @@
+export declare enum Level {
+    LOW = 0,
+    MEDIUM = 1,
+    HIGH = 2
+}
+export type Callback = () => void;
