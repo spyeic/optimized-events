2ee7cf28e4462010b1467c084c9e1a1dd44a2951
Eric <lderic0415@gmail.com>
2023-11-11 19:44:08 -0800

    add ignore dir

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
         "es2021": true,
         "node": true
     },
-    "ignorePatterns": ["./dist/**.*"],
+    "ignorePatterns": ["/dist"],
     "extends": [
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
