36bbd39e220d552f70ea6e03e5dc14f477c7d69a
Eric <lderic0415@gmail.com>
2023-11-11 21:53:24 -0800

    add dist to git ignore

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import Listener from "./listener";
-import OptimizedEvents from "./main";
-import { Callback, Level } from "./types";
-export default OptimizedEvents;
-export { Listener, Callback, Level };
--- dist/types.d.ts
+++
@@ -1,6 +0,0 @@
-export declare enum Level {
-    LOW = 0,
-    MEDIUM = 1,
-    HIGH = 2
-}
-export type Callback = () => void;
